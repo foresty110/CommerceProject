ca0bfb8011962f6fea425f2504ba70bffc730466
heo <foresty110@gmail.com>
2025-09-22 14:07:59 +0900

    update: Product 클래스를 생성하여 목록 추가, List타입으로 상품 관리

--- src/main/java/commerce/Main.java
+++ src/main/java/commerce/Main.java
@@ -1,19 +1,36 @@
 package commerce;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]" +
-                "\n1. Galaxy S25     | 1,200,000원 | 최신 안드로이드 스마트폰" +
-                "\n2. iPhone 16      | 1,350,000원 | Apple의 최신 스마트폰" +
-                "\n3. MacBook Pro    | 2,400,000원 | M3 칩셋이 탑재된 노트북" +
-                "\n4. AirPods Pro    |   350,000원 | 노이즈 캔슬링 무선 이어폰" +
-                "\n0. 종료            | 프로그램 종료"
-        );
+        // 전자제품 데이터 저장
+        List<Product> products = new ArrayList<>();
+        Product p1 = new Product("Galaxy S25", 1200000, "Apple의 최신 스마트폰");
+        Product p2 = new Product("iPhone 16", 1350000, "최신 안드로이드 스마트폰");
+        Product p3 = new Product("MacBook Pro", 2400000, "M3 칩셋이 탑재된 노트북");
+        Product p4 = new Product("AirPods Pro", 350000, "노이즈 캔슬링 무선 이어폰");
 
+        products.add(p1);
+        products.add(p2);
+        products.add(p3);
+        products.add(p4);
+
+        // 전자제품 목록 출력
+        System.out.println("[ 실시간 커머스 플랫폼 - 전자제품 ]");
+
+        int count = 1;
+        for (Product p : products) {
+            System.out.println(count++ + ". " + p );
+        }
+
+        System.out.println("0. 종료 \t\t\t | 프로그램 종료");
+
+        // 사용자로부터 입력받기
         Scanner scanner = new Scanner(System.in);
 
         try {
@@ -28,5 +45,6 @@ public class Main {
         } catch (InputMismatchException e) {
             System.out.println("숫자를 입력해 주세요");
         }
+
     }
 }
--- src/main/java/commerce/Product.java
+++ src/main/java/commerce/Product.java
@@ -1,4 +1,22 @@
 package commerce;
 
 public class Product {
+    //TODO
+    private String name;
+    private int price;
+    private String description;
+    private int quantity;
+
+    public Product() {
+
+    }
+    public Product(String name, int price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public String toString(){
+        return this.name + "\t |" + this.price + "\t |" + this.description;
+    }
 }
